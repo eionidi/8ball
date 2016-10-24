1c08223fb45ebab68b9ce2bc9ef26a11b4a84b1c
Екатерина Иониди <ekaterinaionidi@MacBook-Pro-Ekaterina.local>
2016-10-24 22:31:38 +0300

    initial commit

---
+++ 8ball.rb
@@ -0,0 +1,43 @@
+
+answers = [
+
+  #Положительные
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes — definitely",
+  "You may rely on it",
+
+  #Нерешительно положительные
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Signs point to yes",
+  "Yes",
+
+  #Нейтральные
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+
+  #Отрицательные
+  "Don’t count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+]
+
+greetings = [
+  "Кто вопрошает, тот получит ответ:",
+  "Здравствуй, смертный. Сегодня для тебя такой ответ:",
+  "Привет, дорогой друг. Отвечаю на твой вопрос..."
+]
+
+puts greetings.sample
+puts
+#выводим случайный элемент массива
+sleep (3)
+puts answers.sample
