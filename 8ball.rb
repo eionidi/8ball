f2b814db6f22c9b940b904a66a08f8f0f45aa347
Ekaterina Ionidi <cat81.06@gmail.com>
2016-10-25 22:57:58 +0300

    Update 8ball.rb
    refactoring

--- 8ball.rb
+++ 8ball.rb
@@ -1,4 +1,3 @@
-
 answers = [
   #Положительные
   "It is certain",
@@ -6,18 +5,21 @@ answers = [
   "Without a doubt",
   "Yes — definitely",
   "You may rely on it",
+
   #Нерешительно положительные
   "As I see it, yes",
   "Most likely",
   "Outlook good",
   "Signs point to yes",
   "Yes",
+
   #Нейтральные
   "Reply hazy, try again",
   "Ask again later",
   "Better not tell you now",
   "Cannot predict now",
   "Concentrate and ask again",
+
   #Отрицательные
   "Don’t count on it",
   "My reply is no",
@@ -25,13 +27,16 @@ answers = [
   "Outlook not so good",
   "Very doubtful"
 ]
+
 greetings = [
   "Кто вопрошает, тот получит ответ:",
   "Здравствуй, смертный. Сегодня для тебя такой ответ:",
   "Привет, дорогой друг. Отвечаю на твой вопрос..."
 ]
+
 puts greetings.sample
 puts
+
 #выводим случайный элемент массива
 sleep (3)
 puts answers.sample
