403f952d982236c9326c61fa674f580a30e43f4e
Ekaterina Ionidi <cat81.06@gmail.com>
2016-10-25 11:51:51 +0300

    Update 8ball.rb
    delete spaces

--- 8ball.rb
+++ 8ball.rb
@@ -1,27 +1,23 @@
 
 answers = [
-
   #Положительные
   "It is certain",
   "It is decidedly so",
   "Without a doubt",
   "Yes — definitely",
   "You may rely on it",
-
   #Нерешительно положительные
   "As I see it, yes",
   "Most likely",
   "Outlook good",
   "Signs point to yes",
   "Yes",
-
   #Нейтральные
   "Reply hazy, try again",
   "Ask again later",
   "Better not tell you now",
   "Cannot predict now",
   "Concentrate and ask again",
-
   #Отрицательные
   "Don’t count on it",
   "My reply is no",
@@ -29,16 +25,13 @@ answers = [
   "Outlook not so good",
   "Very doubtful"
 ]
-
 greetings = [
   "Кто вопрошает, тот получит ответ:",
   "Здравствуй, смертный. Сегодня для тебя такой ответ:",
   "Привет, дорогой друг. Отвечаю на твой вопрос..."
 ]
-
 puts greetings.sample
 puts
-
 #выводим случайный элемент массива
 sleep (3)
 puts answers.sample
